c3092fc70ed2953f9575e61b0bad9611137bf0b4
Marc Wilhite <marc.wilhite@gmail.com>
2014-09-23 14:00:02 -0700

    Buffer on page load

--- js/audio.js
+++ js/audio.js
@@ -11,6 +11,7 @@ if (contextClass) {
   $("body").append("<h2>Browser unsupported! Please upgrade to a modern browser.");
 }
 
+
 var analyser = context.createAnalyser();
 analyser.minDecibels = -90;
 analyser.maxDecibels = 0;
@@ -23,5 +24,8 @@ $("audio").each(function(id, element){
   var source = context.createMediaElementSource(element);
   source.connect(context.destination);
   source.connect(analyser);
+  element.play();
+  setTimeout(function(){element.pause();}, 10);
 });
 var audioElement = $("#event")[0];
+
--- js/visualizer.js
+++ js/visualizer.js
@@ -143,3 +143,4 @@ $(document).ready(function() {
   });
 
 });
+
